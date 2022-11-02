a701a6c7708a05420b5fe12b07997529d0e9b271
Lucas Kent <unspeakablefan15@gmail.com>
2022-11-02 18:16:19 -0400

    Broken Thing in teh website
    I deleted the broken things

--- tools/mathisfun/www.mathsisfun.com/games/a/sokoban/js/rss.js/Rss.Random.js
+++
@@ -1,2 +0,0 @@
-'use strict';Rss.Random={};Rss.Random.index=function(array){return Math.floor(Math.random()*array.length);}
-Rss.Random.choice=function(array){return array[Rss.Random.index(array)];}
--- tools/mathisfun/www.mathsisfun.com/games/chess/fancy_chess_6k.js
+++
@@ -1,1 +0,0 @@
-function about(){if(document.getElementById('tableAbout').style.display=='block'){document.getElementById('tableAbout').style.display='none';}else{document.getElementById('tableAbout').style.display='block';}};
